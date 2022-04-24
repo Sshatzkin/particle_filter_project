d9b5f870e98a03d74709c042d9ab3fb5e5b168ae
Sam Shatzkin <sshatzkin@uchicago.edu>
2022-04-24 18:05:55 -0500

    CHanged num

--- scripts/particle_filter.py
+++ scripts/particle_filter.py
@@ -112,7 +112,7 @@ class ParticleFilter:
         self.map = OccupancyGrid()
 
         # the number of particles used in the particle filter
-        self.num_particles = 5
+        self.num_particles = 20
 
         # initialize the particle cloud array
         self.particle_cloud = []
