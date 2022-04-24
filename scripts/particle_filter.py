4d62710af769ea5425bdb4aaf7bd493a1bb6665f
Sam <sshatzkin@uchicago.edu>
2022-04-24 16:46:21 -0500

    Changed something?

--- scripts/particle_filter.py
+++ scripts/particle_filter.py
@@ -47,8 +47,8 @@ def movement_model(self, delta_x, delta_y, curr_yaw, old_yaw):
 
         for particle in self.particle_cloud:
             
-            particle.pose.position.x = particle.position.x + translation * math.cos(old_yaw + rotation_1)
-            particle.pose.position.y = particle.position.y + translation * math.sin(old_yaw + rotation_1)
+            particle.pose.position.x = particle.pose.position.x + translation * math.cos(old_yaw + rotation_1)
+            particle.pose.position.y = particle.pose.position.y + translation * math.sin(old_yaw + rotation_1)
             quat = quaternion_from_euler(0, 0, old_yaw + rotation_1 + rotation_2)
             particle.pose.orientation = Quaternion(quat[0], quat[1], quat[2], quat[3])
             
